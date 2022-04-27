14196633a02ac2b5131a47554cce4eb4f3213fdc
ERIC_EX <eric_long_him_ko@brown.edu>
2022-04-27 13:51:01 -0400

    first draft

--- index.py
+++ index.py
@@ -19,25 +19,21 @@ class Indexer:
         self.docs_path = doc
         self.words_path = word
 
+        # title dic: id to title
+        self.title_dict = {}
+        # relevance dic: words to dic of pages to relevance
         self.relevance_dict = {}
 
-        # # pagerank
-        # self.previous = {}  # id --> rank r
-        # self.current = {}  # id --> rank r'
-
-        # # pagerank
-        # self.links_dict = {}
-        # # pagerank
-        # self.title_to_id = {}
-
-        # # title dic
-        # self.title_dict = {}
+        # pagerank id to title
+        self.links_dict = {}
 
         self.parser()
-        # self.page_rank()
+        self.page_rank()
         self.write_files()
 
     def parser(self):
+        '''Populates the rel dic: dictionary of words to dictionary of documents to relevance
+        '''
         n_regex = '''\[\[[^\[]+?\]\]|[a-zA-Z0-9]+'[a-zA-Z0-9]+|[a-zA-Z0-9]+'''
         stop_words = set(stopwords.words('english'))
         make_stems = PorterStemmer()
@@ -47,15 +43,21 @@ class Indexer:
         self.root = et.parse(self.xml_path).getroot()
         self.all_pages = self.root.findall("page")
         
-        num_of_pages = 0
+        self.num_of_pages = 0
         for page in self.all_pages:
+            page_id = int(page.find('id').text)
+            title = page.find('title').text
+            # for title dic loops through each page and adds page id to coresponding title 
+            self.title_dict[page_id] = title.strip()
+            # for pagerank keep track of id to set of pages (through their title)
+            self.links_dict[page_id] = set()
+            
             # for tf
             aj_max_count = 0
             set_of_words_in_this_page = set()
-            title = re.findall(n_regex, page.find('title').text)
+            title_text = re.findall(n_regex, title)
             all_text = re.findall(n_regex, page.find('text').text)
-            all_text.extend(title)
-            page_id = int(page.find('id').text)
+            all_text.extend(title_text)
             for word in all_text:
                 # strip links
                 word.strip("[[ ]]")
@@ -63,9 +65,12 @@ class Indexer:
                     continue
                 # case |
                 elif "|" in word:
+                    self.links_dict[page_id].add(word[:word.find("|")])
                     list = re.findall(n_regex, word[word.find("|") + 1:])  
+
                 # case :
                 elif ":" in word:
+                    self.links_dict[page_id].add(word)
                     list = re.findall(n_regex, word)
                 # case not link
                 else:
@@ -90,34 +95,16 @@ class Indexer:
             for wordd in set_of_words_in_this_page:
                 tf = self.relevance_dict[wordd][page_id]/aj_max_count
                 self.relevance_dict[wordd][page_id] = tf 
-            num_of_pages += 1
+            self.num_of_pages += 1
         for word in self.relevance_dict:
             num_of_page_for_word = len(self.relevance_dict[word])
             for doc in self.relevance_dict[word]:
-                self.relevance_dict[word][doc] *= math.log(num_of_pages/num_of_page_for_word)
-
-
-            # loops through each page and adds page id and title to the title dic
-            # self.title_dict[int(page.find('id').text)] = page.find(
-            #     'title').text.strip()
-
-            # self.title_to_id[page.find('title').text] = int(
-            #     page.find('id').text)
-
-            # self.links_dict[int(page.find('id').text)] = set()
-            # all_links = re.findall(link_regex, page.find('text').text)
-            # for link in all_links:
-            #     stripped_link = link.strip("[[ ]]")
-            #     if "|" in stripped_link:
-            #         self.links_dict[int(page.find('id').text)].add(
-            #             self.title_to_id[stripped_link.partition("|")[0]])
-            #     else:
-            #         self.links_dict[int(page.find('id').text)].add(
-            #             self.title_to_id[stripped_link])
+                self.relevance_dict[word][doc] *= math.log(self.num_of_pages/num_of_page_for_word)
 
 
     def page_rank(self):
-
+        self.previous = {}  # id --> rank r
+        self.current = {}  # id --> rank r'
         for page in self.all_pages:
             self.previous[int(page.find('id').text)] = 0
 
@@ -133,15 +120,17 @@ class Indexer:
                         k, j) * self.previous[int(k.find('id').text)]
 
     def compute_weights(self, page1: str, page2: str):
-
-        if int(page1.find('id').text) == int(page2.find('id').text):
-            return 0.15/len(self.all_pages)
-        elif len(self.links_dict[int(page1.find('id').text)]) == 0:
-            return 0.15/len(self.all_pages) + (1 - 0.15)*(1/(len(self.all_pages) - 1))
-        elif int(page2.find('id').text) in self.links_dict[int(page1.find('id').text)]:
-            return 0.15/len(self.all_pages) + (1 - 0.15)*(1/len(self.links_dict[int(page1.find('id').text)]))
-        elif int(page2.find('id').text) not in self.links_dict[int(page1.find('id').text)]:
-            return 0.15/len(self.all_pages)
+        page1_id = int(page1.find('id').text)
+        page2_id = int(page2.find('id').text)
+        page2_title = page2.find('title').text
+        if page1_id == page2_id:
+            return 0.15/self.num_of_pages
+        elif len(self.links_dict[page1_id]) == 0:
+            return 0.15/self.num_of_pages + (1 - 0.15)*(1/(self.num_of_pages - 1))
+        elif page2_title in self.links_dict[int(page1.find('id').text)]:
+            return 0.15/self.num_of_pages + (1 - 0.15)*(1/len(self.links_dict[int(page1.find('id').text)]))
+        elif page2_title not in self.links_dict[int(page1.find('id').text)]:
+            return 0.15/self.num_of_pages
 
     def compute_dist(self, previous: dict, current: dict):
         prev = []
@@ -153,10 +142,9 @@ class Indexer:
         return math.dist(curr, prev)
 
     def write_files(self):
-        # file_io.write_title_file(
-        #     self.title_path, self.title_dict)  # writing to title but not to the other two?
+        file_io.write_title_file(self.title_path, self.title_dict) 
         file_io.write_words_file(self.words_path, self.relevance_dict)
-        # file_io.write_docs_file(self.docs_path, self.current)
+        file_io.write_docs_file(self.docs_path, self.current)
 
 
 if __name__ == "__main__":
